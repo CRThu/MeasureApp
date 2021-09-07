b0a40781ad5fed657f904c66e404933172ea3c7b
Carrot <11172391+CRThu@users.noreply.github.com>
2021-09-07 17:05:27 +0800

    添加项目文件。

---
+++ WpfApp1/GPIB.cs
@@ -0,0 +1,71 @@
+﻿using NationalInstruments.VisaNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class GPIB
+    {
+        public bool isOpen = false;
+        public string deviceAddr = string.Empty;
+        public MessageBasedSession messageBasedSession;
+
+        public GPIB()
+        {
+        }
+
+        public GPIB(string deviceAddr)
+        {
+            this.deviceAddr = deviceAddr;
+        }
+
+
+        // Search Devices
+        // Reference: documentation.help/VISA.NET/VISA_Attibutes.htm
+        // INSTR: Instrument Control
+        // INTFC: GPIB Bus Interface
+        public static string[] SearchDevices(string expression = "GPIB?*INSTR")
+        {
+            return ResourceManager.GetLocalManager().FindResources(expression);
+        }
+
+        public void Open()
+        {
+            Open(deviceAddr);
+        }
+
+        public void Open(string deviceAddr)
+        {
+            messageBasedSession = (MessageBasedSession)ResourceManager.GetLocalManager().Open(deviceAddr);
+            isOpen = true;
+            this.deviceAddr = deviceAddr;
+        }
+
+        public void Dispose()
+        {
+            isOpen = false;
+            deviceAddr = string.Empty;
+            if (messageBasedSession != null)
+            {
+                messageBasedSession.Dispose();
+            }
+        }
+
+        public void Write(string cmd)
+        {
+            if (isOpen)
+            {
+                messageBasedSession.Write(cmd);
+            }
+        }
+
+        public string Query(string cmd)
+        {
+            return isOpen ? messageBasedSession.Query(cmd).Trim() : null;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NationalInstruments.VisaNS;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GPIB gpib = new GPIB();
+        public ObservableCollection<string> gpibDeviceNames = new ObservableCollection<string>(GPIB.SearchDevices());
+        public MainWindow()
+        {
+            InitializeComponent();
+            deviceComboBox.ItemsSource = gpibDeviceNames;
+
+            if (gpibDeviceNames.Count != 0 && deviceComboBox.SelectedIndex == -1)
+            {
+                deviceComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void SearchGPIBDevicesButton_Click(object sender, RoutedEventArgs e)
+        {
+            gpibDeviceNames.Clear();
+            foreach (string resource in GPIB.SearchDevices())
+            {
+                gpibDeviceNames.Add(resource);
+            }
+
+            if (gpibDeviceNames.Count != 0 && deviceComboBox.SelectedIndex == -1)
+            {
+                deviceComboBox.SelectedIndex = 0;
+            }
+        }
+        private void OpenGPIBDeviceButton_Click(object sender, RoutedEventArgs e)
+        {
+            gpib.Dispose();
+            gpib.Open((string)deviceComboBox.SelectedItem);
+            gpib.Write("END");
+            string deviceName = gpib.Query("ID?");
+            deviceNameLabel.Content = deviceName;
+        }
+
+        private void WriteCmdButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (gpib.isOpen)
+            {
+                gpib.Write(writeCmdTextBox.Text);
+            }
+            else
+            {
+                _ = MessageBox.Show("GPIB is not open.");
+            }
+        }
+
+        private void QueryCmdButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (gpib.isOpen)
+            {
+                readCmdTextBox.Text = gpib.Query(writeCmdTextBox.Text);
+            }
+            else
+            {
+                _ = MessageBox.Show("GPIB is not open.");
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            gpib.Dispose();
+        }
+
+    }
+}
