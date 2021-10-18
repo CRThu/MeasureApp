568f69ed3bb9ea52f84198fe170b478059e4900a
Carrot <11172391+CRThu@users.noreply.github.com>
2021-10-18 17:44:05 +0800

    修复绑定bug

--- MeasureApp/ViewModel/SerialPortsStatusTextConverter.cs
+++ MeasureApp/ViewModel/SerialPortsStatusTextConverter.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using MeasureApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO.Ports;
+using System.Windows.Data;
 
 namespace MeasureApp
 {
-    public class SerialPortsStatusTextConverter
+    public class SerialPortsStatusTextConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // TODO
+            // TODO MultiBinding
             //Select(serialPort => $"{serialPort.PortName}({serialPort.BaudRate}bps)").ToArray()) + "\nDevice Connected.";
-            // return string.Join(",\n", value as Dictionary<string, dynamic>.KeyCollection);
-            return "TEST";
+            return value is Dictionary<string, SerialPort>.KeyCollection && (value as Dictionary<string, SerialPort>.KeyCollection).Count != 0
+                ? string.Join(",\n", value as Dictionary<string, SerialPort>.KeyCollection) + "\nDevice Connected."
+                : "No Device Connected.";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
