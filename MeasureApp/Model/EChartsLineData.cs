99dd03011f621301f48721f1eab1aa56ca44e563
Carrot <11172391+CRThu@users.noreply.github.com>
2022-01-25 16:42:34 +0800

    EChartsLineData封装

---
+++ MeasureApp/Model/EChartsLineData.cs
@@ -0,0 +1,76 @@
+﻿using MeasureApp.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MeasureApp.Model
+{
+    public class EChartsLineData : NotificationObjectBase
+    {
+        // 图表标题
+        private string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                RaisePropertyChanged(() => Title);
+            }
+        }
+
+        // 图表数据
+        private Dictionary<string, ObservableRangeCollection<double>> data;
+        public Dictionary<string, ObservableRangeCollection<double>> Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                RaisePropertyChanged(() => Data);
+            }
+        }
+
+        // 图表配置
+        private Dictionary<string, string> series;
+        public Dictionary<string, string> Series
+        {
+            get => series;
+            set
+            {
+                series = value;
+                RaisePropertyChanged(() => Series);
+            }
+        }
+
+        public EChartsLineData()
+        {
+            Data = new()
+            {
+                { "x", new ObservableRangeCollection<double>() },
+                { "y", new ObservableRangeCollection<double>() }
+            };
+            Series = new()
+            {
+                { "Name", "Data" },
+                { "Type", "line" },
+                { "DataXColumnsName", "x" },
+                { "DataYColumnsName", "y" }
+            };
+        }
+
+        public EChartsLineData(double[] x, double[] y) : this()
+        {
+            Data["x"] = new ObservableRangeCollection<double>(x);
+            Data["y"] = new ObservableRangeCollection<double>(y);
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
--- MeasureApp/ViewModel/MainWindowDataContextEChartsTab.cs
+++ MeasureApp/ViewModel/MainWindowDataContextEChartsTab.cs
@@ -1,8 +1,10 @@
-﻿using MeasureApp.Model.Common;
+﻿using MeasureApp.Model;
+using MeasureApp.Model.Common;
 using MeasureApp.Model.Converter;
 using MeasureApp.ViewModel.Common;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,16 +43,18 @@ namespace MeasureApp.ViewModel
             switch ((string)param)
             {
                 case "0":
-                    js = "{title:\"Chart\",Data:{\"x\":[1,2,3,4,5,6,7,8,9,10],\"y\":[1,4,9,16,25,36,49,64,81,100]},Series:{Name:\"a\",Type:\"line\",DataXColumnsName:\"x\",DataYColumnsName:\"y\"}}";
+                    js = "{Title:\"Chart\",Data:{\"x\":[1,2,3,4,5,6,7,8,9,10],\"y\":[1,4,9,16,25,36,49,64,81,100]},Series:{Name:\"a\",Type:\"line\",DataXColumnsName:\"x\",DataYColumnsName:\"y\"}}";
                     ChartData1 = js;
                     break;
                 case "1":
-                    js = "{title:\"Chart\",Data:{\"x\":[1,2,3,4,5,6,7,8,9,10],\"y\":[1,0.5,0.33,0.25,0.2,0.17,0.14,0.13,0.11,0.1]},Series:{Name:\"a\",Type:\"line\",DataXColumnsName:\"x\",DataYColumnsName:\"y\"}}";
+                    js = "{Title:\"Chart\",Data:{\"x\":[1,2,3,4,5,6,7,8,9,10],\"y\":[1,0.5,0.33,0.25,0.2,0.17,0.14,0.13,0.11,0.1]},Series:{Name:\"a\",Type:\"line\",DataXColumnsName:\"x\",DataYColumnsName:\"y\"}}";
                     ChartData2 = js;
                     break;
                 default:
                     throw new NotImplementedException();
             }
+            EChartsLineData eChartsLineData = new(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new double[] { 1, 0.5, 0.33, 0.25, 0.2, 0.17, 0.14, 0.13, 0.11, 0.1 });
+            Debug.WriteLine(eChartsLineData.ToJson());
         }
 
 
