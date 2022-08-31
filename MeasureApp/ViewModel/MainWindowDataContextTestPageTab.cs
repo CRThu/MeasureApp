157553b33d0cda986ed0daf4f7feaeee0bdc936f
Carrot <11172391+CRThu@users.noreply.github.com>
2022-08-31 16:30:59 +0800

    ScottPlot图表测试

--- MeasureApp/ViewModel/MainWindowDataContextTestPageTab.cs
+++ MeasureApp/ViewModel/MainWindowDataContextTestPageTab.cs
@@ -11,10 +11,43 @@ using System.Windows;
 using MeasureApp.Model.Common;
 using System.Collections.ObjectModel;
 using RoslynPad.Roslyn;
+using ScottPlot;
+using MeasureApp.ViewModel.Common;
 
 namespace MeasureApp.ViewModel
 {
     public partial class MainWindowDataContext : NotificationObjectBase
     {
+        private WpfPlot plotControl = new();
+        public WpfPlot PlotControl
+        {
+            get => plotControl;
+            set
+            {
+                plotControl = value;
+                RaisePropertyChanged(() => PlotControl);
+            }
+        }
+
+        public void PlotUpdate()
+        {
+            double[] values = DataGen.RandomWalk(1_000_000);
+            PlotControl.Plot.AddSignal(values, sampleRate: 48_000);
+            PlotControl.Plot.Title("One Million Points");
+            PlotControl.Refresh();
+        }
+
+        private CommandBase plotUpdateEvent;
+        public CommandBase PlotUpdateEvent
+        {
+            get
+            {
+                if (plotUpdateEvent == null)
+                {
+                    plotUpdateEvent = new CommandBase(new Action<object>(param => PlotUpdate()));
+                }
+                return plotUpdateEvent;
+            }
+        }
     }
 }
