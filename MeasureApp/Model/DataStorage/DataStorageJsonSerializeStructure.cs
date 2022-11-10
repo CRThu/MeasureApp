49a2bcb6719c508d222b734f1a68e1f941d2eab3
Carrot <11172391+CRThu@users.noreply.github.com>
2022-11-10 15:28:20 +0800

    支持旧格式json数据导入

--- MeasureApp/Model/DataStorage/DataStorageJsonSerializeStructure.cs
+++ MeasureApp/Model/DataStorage/DataStorageJsonSerializeStructure.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace MeasureApp.Model.DataStorage
 {
-    public class DataStorageJsonSerializeStructure : Dictionary<string, Dictionary<string, List<double>>>
+    //public class DataStorageJsonSerializeStructure : Dictionary<string, Dictionary<string, List<double>>>
+    public class DataStorageJsonSerializeStructure : Dictionary<string, JToken>
     {
         public DataStorageJsonSerializeStructure() : base()
         {
@@ -16,11 +18,15 @@ namespace MeasureApp.Model.DataStorage
         {
             foreach (var item in ds.Data)
             {
-                Add(item.Key, new Dictionary<string, List<double>>()
+                var xyArray = new Dictionary<string, List<double>>()
                 {
                     { "X", new List<double>(item.Value.X) },
                     { "Y", new List<double>(item.Value.Y) },
-                });
+                };
+                var jxyArray = JToken.FromObject(xyArray);
+
+
+                Add(item.Key, jxyArray);
             }
         }
 
@@ -29,9 +35,59 @@ namespace MeasureApp.Model.DataStorage
             Dictionary<string, DataStorageElement> data = new();
             foreach (var item in this)
             {
-                DataStorageElement d = new();
-                d.AddXY(item.Value["X"], item.Value["Y"]);
-                data.Add(item.Key, d);
+                JToken dataObj = item.Value;
+                if (dataObj is JArray)
+                {
+                    // 旧版本json文件解析
+                    // 格式为 { "k0": [v0, v1, v2], "k1": [v0, v1, v2] }
+                    /*
+                    {
+                        "aldo_trim": [
+                            1.659,
+                            1.650,
+                            1.641
+                        ],
+                        "dldo_trim": [
+                            1.666,
+                            1.657,
+                            1.648
+                        ]
+                    }
+                                        */
+
+                    DataStorageElement ele0 = new();
+                    var yArray0 = ((JArray)dataObj).Values<double>();
+                    ele0.AddY(yArray0);
+                    data.Add(item.Key, ele0);
+                }
+                else
+                {
+                    // 新版本json文件解析
+                    // 格式为 { "k0": { "X":[v0, v1, v2], "Y":[v0, v1, v2]},
+                    //          "k1": { "X":[v0, v1, v2], "Y":[v0, v1, v2]} }
+                    /*
+                    {
+                        "Key1": {
+                            "X": [1,2,3],
+                            "Y": [4,5,6]
+                        },
+                        "Key2": {
+                            "X": [1,2,3],
+                            "Y": [1,4,9]
+                        },
+                        "Key3": {
+                            "X": [1,2,3],
+                            "Y": [1,8,27]
+                        }
+                    }
+                                            */
+                    var dataDict = new Dictionary<string, JToken>(((JObject)dataObj));
+                    var xArray = ((JArray)dataDict["X"]).Values<double>();
+                    var yArray = ((JArray)dataDict["Y"]).Values<double>();
+                    DataStorageElement d = new();
+                    d.AddXY(xArray, yArray);
+                    data.Add(item.Key, d);
+                }
             }
             return data;
         }
