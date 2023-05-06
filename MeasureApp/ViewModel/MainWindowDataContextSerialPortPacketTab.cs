e67dac640fd2877ba5310e9d571280214b2dfc08
Carrot <11172391+CRThu@users.noreply.github.com>
2023-05-06 15:03:26 +0800

    串口数据包收发界面重画

--- MeasureApp/ViewModel/MainWindowDataContextSerialPortPacketTab.cs
+++ MeasureApp/ViewModel/MainWindowDataContextSerialPortPacketTab.cs
@@ -25,118 +25,37 @@ namespace MeasureApp.ViewModel
             }
         }
 
-        // 字长
-        private int spPktCommTabWordLen = 4;
-        public int SpPktCommTabWordLen
+        public string[] SpPktCommTabProtocolList { get; } = new[] { "Ascii", "Data256", "Bin" };
+
+        // 协议选择
+        private string spPktCommTabProtocolName;
+        public string SpPktCommTabProtocolName
         {
-            get => spPktCommTabWordLen;
+            get => spPktCommTabProtocolName;
             set
             {
-                spPktCommTabWordLen = value;
-                RaisePropertyChanged(() => SpPktCommTabWordLen);
+                spPktCommTabProtocolName = value;
+                RaisePropertyChanged(() => SpPktCommTabProtocolName);
             }
         }
 
-        // 数据包长
-        private int spPktCommTabPktLen = 256;
-        public int SpPktCommTabPktLen
+        // 发送数据文本
+        private string spPktCommTabTxText;
+        public string SpPktCommTabTxText
         {
-            get => spPktCommTabPktLen;
+            get => spPktCommTabTxText;
             set
             {
-                spPktCommTabPktLen = value;
-                RaisePropertyChanged(() => SpPktCommTabPktLen);
+                spPktCommTabTxText = value;
+                RaisePropertyChanged(() => SpPktCommTabTxText);
             }
-        }
-
-        // 数据解析类型
-        private string[] pktCvtTypes = new[]{
-            "System.UInt16",
-            "System.UInt32",
-            "System.String",
-        };
-        public string[] PktCvtTypes
-        {
-            get => pktCvtTypes;
-        }
-
-        private string spPktCommTabSelectedPktCvtType;
-        public string SpPktCommTabSelectedPktCvtType
-        {
-            get => spPktCommTabSelectedPktCvtType;
-            set
-            {
-                spPktCommTabSelectedPktCvtType = value;
-                RaisePropertyChanged(() => SpPktCommTabSelectedPktCvtType);
-            }
-        }
-
-        private ObservableCollection<string> spPktCommTabForEachGenerateStatments;
-        public ObservableCollection<string> SpPktCommTabForEachGenerateStatments
-        {
-            get => spPktCommTabForEachGenerateStatments;
-            set
-            {
-                spPktCommTabForEachGenerateStatments = value;
-                RaisePropertyChanged(() => SpPktCommTabForEachGenerateStatments);
-            }
-        }
-
-        // 读取数据包
-        public string SpPktCommTabReadPkt(string command, int wordLen, int pktLen)
-        {
-            byte[] pktData = SerialPortsInstance.GetDataPacket(SpPktCommTabSerialPortName, command, wordLen * pktLen);
-            return BytesConverter.Bytes2Hex(pktData);
-        }
-
-        public T[] SpPktCommTabReadPkt<T>(string command, int wordLen, int pktLen) where T : struct
-        {
-            string data = SpPktCommTabReadPkt(command, wordLen, pktLen);
-            return BytesConverter.FromBytes<T>(data);
-        }
-
-        public object SpPktCommTabReadPkt(string command, int wordLen, int pktLen, Type type)
-        {
-            if (type == typeof(string))
-            {
-                string bytesPacket = SpPktCommTabReadPkt(command, wordLen, pktLen);
-                string[] bytes = new string[bytesPacket.Length / SpPktCommTabWordLen / 2];
-                for (int i = 0; i < bytes.Length; i += 1)
-                    bytes[i] = bytesPacket.Substring(2 * i * SpPktCommTabWordLen, 2 * SpPktCommTabWordLen);
-                return bytes;
-            }
-            else
-                return this.GetType()
-                .GetMethod(nameof(SpPktCommTabReadPkt), 1, new Type[] { typeof(string), typeof(int), typeof(int) })
-                .MakeGenericMethod(type)
-                .Invoke(this, new object[] { command, wordLen, pktLen });
-        }
-
-        public object SpPktCommTabReadPkt(string command, int wordLen, int pktLen, string type)
-        {
-            return SpPktCommTabReadPkt(command, wordLen, pktLen, Type.GetType(type));
         }
 
         public void SpPktCommTabReadPktGUI(object param)
         {
             try
             {
-                string command;
-                int pktLen;
-                if ((string)param == "word")
-                {
-                    command = "PKT;0;";
-                    pktLen = 1;
-                }
-                else
-                {
-                    command = "PKT;1;";
-                    pktLen = SpPktCommTabPktLen;
-                }
-                object dat = SpPktCommTabReadPkt(command, SpPktCommTabWordLen, pktLen, SpPktCommTabSelectedPktCvtType);
-                DataStorageInstance.AddValues(KeySerialPortString, ((dynamic[])dat).Select<dynamic, decimal>(x => Convert.ToDecimal(x)));
-                // TODO
-                MessageBox.Show("函数未验证, MainWindowDataContextSerialPortPacketTab.cs line127");
+
             }
             catch (Exception ex)
             {
