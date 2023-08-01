65dacf52ced047082cc1b7adb03da28e9eb81054
Carrot <11172391+CRThu@users.noreply.github.com>
2023-08-01 17:20:55 +0800

    新增SerialPortDriver

---
+++ CarrotProtocolLib/Driver/SerialPortDriver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotProtocolLib.Driver
+{
+    public class SerialPortDriver
+    {
+        private SerialPort Driver { get; set; }
+
+        public bool IsOpen { get; set; }
+
+        public int BytesToRead => Driver.BytesToRead;
+
+        public SerialPortDriver()
+        {
+            Driver = new SerialPort();
+        }
+
+        public void Open()
+        {
+
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return 0;
+        }
+    }
+}
