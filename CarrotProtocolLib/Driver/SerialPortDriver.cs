fd9636d58d93b64e49101d61285be665713c82c7
Carrot <11172391+CRThu@users.noreply.github.com>
2023-08-02 17:11:54 +0800

    update SerialPortDriver

--- CarrotProtocolLib/Driver/SerialPortDriver.cs
+++ CarrotProtocolLib/Driver/SerialPortDriver.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarrotProtocolLib.Util;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
@@ -15,29 +17,127 @@ namespace CarrotProtocolLib.Driver
 
         public int BytesToRead => Driver.BytesToRead;
 
+        public int ReceivedByteCount { get; set; }
+
+        public int SentByteCount { get; set; }
+
+        public delegate void ErrorReceivedHandler(Exception ex);
+        public event ErrorReceivedHandler? ErrorReceived;
+
         public SerialPortDriver()
         {
             Driver = new SerialPort();
+
+            Driver.ErrorReceived += Driver_ErrorReceived;
         }
 
+        public void SetDevice(string portName, int baudRate,
+            int dataBits, float stopBits, string parity,
+            int bufferSize = 1048576, int timeout = 250)
+        {
+            Driver.PortName = portName;
+            Driver.BaudRate = baudRate;
+            Driver.DataBits = dataBits;
+            Driver.StopBits = SerialPortHelper.StopBitsFloat2Enum(stopBits);
+            Driver.Parity = SerialPortHelper.ParityString2Enum(parity);
+            Driver.ReadBufferSize = bufferSize;
+            Driver.WriteBufferSize = bufferSize;
+            Driver.ReadTimeout = timeout;
+            Driver.WriteTimeout = timeout;
+        }
+
+        /// <summary>
+        /// 串口内部错误捕获
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Driver_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            ErrorReceived?.Invoke(new DriverErrorException(this, $"ErrorReceived, EventType={e.EventType}."));
+            IsOpen = Driver.IsOpen;
+        }
+
+        /// <summary>
+        /// 打开串口
+        /// </summary>
         public void Open()
         {
-
+            try
+            {
+                Driver.Open();
+                IsOpen = Driver.IsOpen;
+            }
+            catch (Exception ex)
+            {
+                IsOpen = Driver.IsOpen;
+                throw new DriverErrorException(this, ex.Message, ex);
+            }
         }
 
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
         public void Close()
         {
-
+            try
+            {
+                Driver.Close();
+                IsOpen = Driver.IsOpen;
+            }
+            catch (Exception ex)
+            {
+                IsOpen = Driver.IsOpen;
+                throw new DriverErrorException(this, ex.Message, ex);
+            }
         }
 
+        /// <summary>
+        /// 写入字节数组
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
         public void Write(byte[] buffer, int offset, int count)
         {
-
+            try
+            {
+                Driver.Write(buffer, offset, count);
+                SentByteCount += count;
+            }
+            catch (Exception ex)
+            {
+                IsOpen = Driver.IsOpen;
+                throw new DriverErrorException(this, ex.Message, ex);
+            }
         }
 
-        public int Read(byte[] buffer, int offset, int count)
+        /// <summary>
+        /// 读取字节数组
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="bytesExpected"></param>
+        /// <returns>返回实际读取字节数</returns>
+        public int Read(byte[] buffer, int offset, int bytesExpected)
         {
-            return 0;
+            try
+            {
+                int bytesRead;
+                while (bytesExpected > 0)
+                {
+                    bytesRead = Driver.Read(buffer, offset, bytesExpected);
+                    offset += bytesRead;
+                    bytesExpected -= bytesRead;
+                }
+                ReceivedByteCount += offset;
+                return offset;
+            }
+            catch (Exception ex)
+            {
+                IsOpen = Driver.IsOpen;
+                throw new DriverErrorException(this, ex.Message, ex);
+            }
         }
     }
+
 }
---
+++ CarrotProtocolLib/Driver/SerialPortHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotProtocolLib.Driver
+{
+    public static class SerialPortHelper
+    {
+        public static StopBits StopBitsFloat2Enum(float stopBits)
+        {
+            return stopBits switch
+            {
+                0f => StopBits.None,
+                1f => StopBits.One,
+                2f => StopBits.Two,
+                1.5f => StopBits.OnePointFive,
+                _ => throw new DriverErrorException(nameof(SerialPortHelper), $"不支持的停止位: {stopBits:0.0}"),
+            };
+        }
+
+        public static Parity ParityString2Enum(string parity)
+        {
+            return parity switch
+            {
+                "None" => Parity.None,
+                "Odd" => Parity.Odd,
+                "Even" => Parity.Even,
+                "Mark" => Parity.Mark,
+                "Space" => Parity.Space,
+                _ => throw new DriverErrorException(nameof(SerialPortHelper), $"不支持的校验位: {parity}"),
+            };
+        }
+    }
+}
