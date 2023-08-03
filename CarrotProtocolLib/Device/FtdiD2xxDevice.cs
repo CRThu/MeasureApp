42ebd72a6b9adbb7049b36b2d59f8939663701a0
Carrot <11172391+CRThu@users.noreply.github.com>
2023-08-03 17:16:07 +0800

    update FtdiD2xxHelper

--- CarrotProtocolLib/Device/FtdiD2xxDevice.cs
+++ CarrotProtocolLib/Device/FtdiD2xxDevice.cs
@@ -26,11 +26,8 @@ namespace CarrotProtocolLib.Device
 
         public int RxByteToRead { get; set; }
 
-        public static EmptyDevice EmptyDeviceInstance { get; } = new EmptyDevice();
         public SerialPort Driver { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public RingBuffer RxBuffer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public event IDevice.DevicePropertyChangedHandler DevicePropertyChanged;
 
         public FtdiD2xxDevice()
         {
@@ -60,63 +57,6 @@ namespace CarrotProtocolLib.Device
             throw new NotImplementedException();
         }
 
-        public static DeviceInfo[] GetDevicesInfo()
-        {
-            UInt32 ftdiDeviceCount = 0;
-            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
-
-            // Create new instance of the FTDI device class
-            FTDI myFtdiDevice = new FTDI();
-            Debug.WriteLine(myFtdiDevice.GetType().Assembly.Location);
-
-            // Determine the number of FTDI devices connected to the machine
-            ftStatus = myFtdiDevice.GetNumberOfDevices(ref ftdiDeviceCount);
-            // Check status
-            if (ftStatus == FTDI.FT_STATUS.FT_OK)
-            {
-                Debug.WriteLine("Number of FTDI devices: " + ftdiDeviceCount.ToString());
-                Debug.WriteLine("");
-            }
-            else
-            {
-                // Wait for a key press
-                Debug.WriteLine("Failed to get number of devices (error " + ftStatus.ToString() + ")");
-                return new DeviceInfo[] { };
-            }
-
-            // If no devices available, return
-            if (ftdiDeviceCount == 0)
-            {
-                // Wait for a key press
-                Debug.WriteLine("Failed to get number of devices (error " + ftStatus.ToString() + ")");
-                return new DeviceInfo[] { };
-            }
-
-            // Allocate storage for device info list
-            FTDI.FT_DEVICE_INFO_NODE[] ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[ftdiDeviceCount];
-
-            // Populate our device list
-            ftStatus = myFtdiDevice.GetDeviceList(ftdiDeviceList);
-
-            if (ftStatus == FTDI.FT_STATUS.FT_OK)
-            {
-                for (UInt32 i = 0; i < ftdiDeviceCount; i++)
-                {
-                    Debug.WriteLine("Device Index: " + i.ToString());
-                    Debug.WriteLine("Flags: " + String.Format("{0:x}", ftdiDeviceList[i].Flags));
-                    Debug.WriteLine("Type: " + ftdiDeviceList[i].Type.ToString());
-                    Debug.WriteLine("ID: " + String.Format("{0:x}", ftdiDeviceList[i].ID));
-                    Debug.WriteLine("Location ID: " + String.Format("{0:x}", ftdiDeviceList[i].LocId));
-                    Debug.WriteLine("Serial Number: " + ftdiDeviceList[i].SerialNumber.ToString());
-                    Debug.WriteLine("Description: " + ftdiDeviceList[i].Description.ToString());
-                    Debug.WriteLine("");
-                }
-            }
-
-            return ftdiDeviceList.Select(dev =>
-            new DeviceInfo("FTD2XX_NET.FTDI_D2XX", dev.SerialNumber, dev.Description))
-                .ToArray();
-        }
 
         public void SetDevice(string serialNumber)
         {
---
+++ CarrotProtocolLib/Driver/FtdiD2xxDriver.cs
@@ -0,0 +1,48 @@
+﻿using CarrotProtocolLib.Device;
+using CarrotProtocolLib.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotProtocolLib.Driver
+{
+    public class FtdiD2xxDriver : IDriver
+    {
+        public bool IsOpen => throw new NotImplementedException();
+
+        public int BytesToRead => throw new NotImplementedException();
+
+        public int ReceivedByteCount => throw new NotImplementedException();
+
+        public int SentByteCount => throw new NotImplementedException();
+
+        public event SerialPortDriver.ErrorReceivedHandler? ErrorReceived;
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeviceInfo[] GetDevicesInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Read(byte[] buffer, int offset, int bytesExpected)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
