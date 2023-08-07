f57cf4cbbd94d3b37517959179856637d0e066df
Carrot <11172391+CRThu@users.noreply.github.com>
2023-08-07 16:43:41 +0800

    update RawAsciiProtocolDecodeService

--- CarrotProtocolLib/Protocol/AsciiProtocol.cs
+++ CarrotProtocolLib/Protocol/AsciiProtocol.cs
@@ -15,77 +15,11 @@ namespace CarrotProtocolLib.Protocol
     {
         public IDevice Device { get; set; }
         public ILogger Logger { get; set; }
-        private CancellationTokenSource ParseProtocolTaskCts { get; set; }
-        private Task<int> ParseProtocolTask { get; set; }
-
-        public event IProtocol.ProtocolParseErrorHandler ProtocolParseError;
 
         public AsciiProtocol(IDevice device, ILogger logger)
         {
             Device = device;
             Logger = logger;
-        }
-
-        public AsciiProtocol(IDevice device, ILogger logger, IProtocol.ProtocolParseErrorHandler protocolParseErrorHandler) : this(device, logger)
-        {
-            ProtocolParseError += protocolParseErrorHandler;
-        }
-
-        public void Start()
-        {
-            Device.Open();
-            ParseProtocolTaskCts = new();
-            ParseProtocolTask = Task.Run(() => ParseProtocolLoop(), ParseProtocolTaskCts.Token);
-        }
-
-        public void Stop()
-        {
-            Device.Close();
-            ParseProtocolTaskCts.Cancel();
-            ParseProtocolTask.Wait();
-        }
-
-        private int ParseProtocolLoop()
-        {
-            try
-            {
-                byte[] rxBuffer = new byte[65536];
-                int rxBufferCursor = 0;
-                int readBytes = 0;
-
-                while (true)
-                {
-                    if (Device.RxByteToRead < 1)
-                    {
-                        Thread.Sleep(10);
-                        //cts.Token.ThrowIfCancellationRequested();
-                        if (ParseProtocolTaskCts.Token.IsCancellationRequested)
-                            return 0;
-                    }
-                    else
-                    {
-                        readBytes = Device.RxByteToRead;
-                        readBytes = readBytes > 4 ? 4 : readBytes;
-                        Device.Read(rxBuffer, rxBufferCursor, readBytes);
-                        int index = Array.IndexOf(rxBuffer, (byte)'\n');
-                        rxBufferCursor += readBytes;
-                        if (index >= 0)
-                        {
-                            byte[] frame = new byte[index + 1];
-                            Array.Copy(rxBuffer, 0, frame, 0, frame.Length);
-                            Array.Copy(rxBuffer, frame.Length, rxBuffer, 0, rxBufferCursor - frame.Length);
-                            Array.Fill(rxBuffer, (byte)0x00, rxBufferCursor - frame.Length, frame.Length);
-                            rxBufferCursor -= frame.Length;
-                            AddLog(frame, 0, frame.Length);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ProtocolParseError?.Invoke(ex);
-                return -1;
-            }
         }
 
         public void Send(IProtocolRecord protocol)
@@ -102,57 +36,6 @@ namespace CarrotProtocolLib.Protocol
         {
             Device.Write(bytes, offset, length);
             Logger.AddTx(new AsciiProtocolRecord(bytes, offset, length));
-        }
-
-        private void AddLog(byte[] bytes, int offset, int length)
-        {
-            AsciiProtocolRecord protocol = new AsciiProtocolRecord(bytes, offset, length);
-            Logger.AddRx(protocol);
-        }
-    }
-
-
-    public partial class AsciiProtocolRecord : ObservableObject, IProtocolRecord
-    {
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(PayloadDisplay))]
-        [NotifyPropertyChangedFor(nameof(Bytes))]
-        private string hexDisplay;
-
-        public string PayloadDisplay
-        {
-            get
-            {
-                return BytesEx.BytesToAscii(Bytes);
-            }
-            set
-            {
-                Bytes = BytesEx.AsciiToBytes(value);
-            }
-        }
-
-        public byte[] Bytes
-        {
-            get
-            {
-                return BytesEx.HexStringToBytes(HexDisplay);
-            }
-            set
-            {
-                HexDisplay = BytesEx.BytesToHexString(value);
-            }
-        }
-
-        public AsciiProtocolRecord(string payload)
-        {
-            Bytes = BytesEx.AsciiToBytes(payload);
-        }
-
-        public AsciiProtocolRecord(byte[] bytes, int offset, int length)
-        {
-            byte[] bytesNew = new byte[length];
-            Array.Copy(bytes, offset, bytesNew, 0, length);
-            Bytes = bytesNew;
         }
     }
 }
---
+++ CarrotProtocolLib/Service/RawAsciiProtocolDecodeService.cs
@@ -0,0 +1,134 @@
+﻿using CarrotProtocolLib.Device;
+using CarrotProtocolLib.Impl;
+using CarrotProtocolLib.Logger;
+using CarrotProtocolLib.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotProtocolLib.Service
+{
+    /// <summary>
+    /// 原始字符串流解析
+    /// </summary>
+    public class RawAsciiProtocolDecodeService : ProtocolDecodeBaseService
+    {
+        /// <summary>
+        /// 操作设备接口
+        /// </summary>
+        private IDevice? Device { get; set; }
+
+        /// <summary>
+        /// 数据帧存储接口
+        /// </summary>
+        private ILogger? Logger { get; set; }
+
+        /// <summary>
+        /// 解码缓冲区(内部使用)
+        /// </summary>
+        private byte[] DecodeBuffer { get; set; }
+
+        /// <summary>
+        /// 解码缓冲区游标(内部使用)
+        /// </summary>
+        private int DecodeBufferCursor { get; set; }
+        /// <summary>
+        /// 解码缓冲区游标,标记有效开始位置(内部使用)
+        /// </summary>
+        private int DecodeBufferStartCursor { get; set; }
+
+        /// <summary>
+        /// 数据帧存储缓冲区(内部使用)
+        /// </summary>
+        private byte[] FrameBuffer { get; set; }
+
+        /// <summary>
+        /// 数据帧存储游标(内部使用)
+        /// </summary>
+        private int FrameBufferCursor { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public RawAsciiProtocolDecodeService() : base()
+        {
+            DecodeBuffer = new byte[64 * 1024];
+            DecodeBufferCursor = 0;
+            FrameBuffer = Array.Empty<byte>();
+            FrameBufferCursor = 0;
+            DecodeBufferStartCursor = 0;
+        }
+
+        /// <summary>
+        /// 数据接收服务程序
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public override int ServiceLoop()
+        {
+            // 大于requiredBytesToRead时进入搜索是否存在字符串结束字符
+            int requiredBytesToRead = 1;
+            // 已搜索过结束字符字符串位置
+            int findFrameEndByteCursor = 0;
+            // 缓冲区有效数据开始位置
+            DecodeBufferStartCursor = 0;
+            while (true)
+            {
+                // 外部退出请求检测
+                if (IsCancellationRequested)
+                    return 1;
+
+                // 获取缓冲区数据
+                int len = Device!.RxByteToRead;
+                // 读取不超过缓冲区空间的字节数
+                if (len > DecodeBuffer.Length - DecodeBufferCursor)
+                    len = DecodeBuffer.Length - DecodeBufferCursor;
+
+                if (len >= requiredBytesToRead)
+                {
+                    // 数据存入DecodeBuffer
+                    Device.Read(DecodeBuffer, DecodeBufferCursor, len);
+                    DecodeBufferCursor += len;
+
+                    // 数据解析
+                    while (findFrameEndByteCursor < DecodeBufferCursor)
+                    {
+                        int index = Array.IndexOf(DecodeBuffer, (byte)'\n', findFrameEndByteCursor);
+
+                        // 查找到结束字符
+                        if (index >= 0)
+                        {
+                            Array.Copy(DecodeBuffer, DecodeBufferStartCursor, FrameBuffer, 0, index + 1 - DecodeBufferStartCursor);
+                            Logger!.AddRx(new AsciiProtocolRecord(FrameBuffer, 0, index + 1 - DecodeBufferStartCursor));
+                            DecodeBufferStartCursor = index + 1;
+                            findFrameEndByteCursor = index + 1;
+                        }
+                        // 未搜索到结束字符
+                        else if (DecodeBufferStartCursor == 0)
+                        {
+                            findFrameEndByteCursor = DecodeBufferCursor;
+                        }
+                        // 已解码多个包，最后一个不完整包无结束字符
+                        else
+                        {
+                            Array.Copy(DecodeBuffer, DecodeBufferStartCursor, DecodeBuffer, 0, DecodeBufferCursor - DecodeBufferStartCursor);
+                            Array.Fill(DecodeBuffer, (byte)0x00, DecodeBufferCursor - DecodeBufferStartCursor, DecodeBuffer.Length - (DecodeBufferCursor - DecodeBufferStartCursor));
+
+                            DecodeBufferCursor -= DecodeBufferStartCursor;
+                            DecodeBufferStartCursor = 0;
+                            findFrameEndByteCursor = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(5);
+                }
+                return 0;
+            }
+
+        }
+    }
+}
