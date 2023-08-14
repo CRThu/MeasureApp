b712b90f2f83767ca2456e6d9f91a3570cc9fe02
Carrot <11172391+CRThu@users.noreply.github.com>
2023-08-14 17:02:33 +0800

    datastorage update

---
+++ CarrotProtocolLib/Logger/DataStorage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotProtocolLib.Logger
+{
+    public class DataStorage<T> where T : struct
+    {
+        public Dictionary<string, List<T>> StorageDict { get; set; }
+
+        /// <summary>
+        /// 线程安全管理
+        /// </summary>
+        private object LockObject;
+
+        public DataStorage()
+        {
+            StorageDict = new();
+        }
+
+        private void CreateKeyIfNotExist(string key)
+        {
+            if (!StorageDict.ContainsKey(key))
+                StorageDict.Add(key, new List<T>());
+        }
+
+        public void AddValue(string key, T value)
+        {
+            lock (LockObject)
+            {
+                CreateKeyIfNotExist(key);
+                StorageDict[key].Add(value);
+            }
+        }
+
+        public void AddValue(string key, IEnumerable<T> value)
+        {
+            lock (LockObject)
+            {
+                CreateKeyIfNotExist(key);
+                StorageDict[key].AddRange(value);
+            }
+        }
+
+        public void RemoveKey(string key)
+        {
+            lock (LockObject)
+            {
+                StorageDict.Remove(key);
+            }
+        }
+
+        public void RemoveValues(string key)
+        {
+            lock (LockObject)
+            {
+                StorageDict[key].Clear();
+            }
+        }
+    }
+}
