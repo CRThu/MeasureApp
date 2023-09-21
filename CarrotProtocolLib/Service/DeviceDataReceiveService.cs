ffef89427a324aa7325b18ac7ea05e3dfa5f94a6
Carrot <11172391+CRThu@users.noreply.github.com>
2023-09-21 18:28:12 +0800

    nlog update

--- CarrotProtocolLib/Service/DeviceDataReceiveService.cs
+++ CarrotProtocolLib/Service/DeviceDataReceiveService.cs
@@ -1,4 +1,7 @@
 ﻿using CarrotProtocolLib.Device;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,7 +24,9 @@ namespace CarrotProtocolLib.Service
         /// <summary>
         /// 接收缓冲区(内部使用)
         /// </summary>
-        private byte[] ReceiveDataBuffer = new byte[64 * 1024];
+        private byte[] ReceiveDataBuffer = new byte[16 * 1024 * 1024];
+
+        public ulong Counter = 0;
 
         /// <summary>
         /// 构造函数
@@ -37,12 +42,28 @@ namespace CarrotProtocolLib.Service
         /// <exception cref="InvalidOperationException"></exception>
         public override int ServiceLoop()
         {
+#if DEBUG
+            var Logger = NLog.LogManager.Setup().LoadConfiguration(builder =>
+            {
+                //builder.ForLogger().WriteToDebug();
+                builder.ForLogger().WriteToFile($"App_{DateTime.Now:yyyyMMddHHmmss}.log",
+                    "${uppercase:${level}} | ${message}")
+                    .WithAsync(NLog.Targets.Wrappers.AsyncTargetWrapperOverflowAction.Grow);
+            }).GetCurrentClassLogger();
+
+            Logger.Debug($"log start");
+
+            //for (int i = 0; i < 10000; i++)
+            //    Logger.Debug($"nlog test {DateTime.Now:HH:mm:ss.ffffff} : read = {0}, counter = {i}");
+#endif
+
             while (Device!.Driver!.IsOpen)
             {
                 if (IsCancellationRequested)
                     return 1;
 
                 int len = Device.Driver.BytesToRead;
+                // Debug.WriteLine($"{DateTime.Now:HH:mm:ss.fff} : BytesToRead = {len}");
 
                 if (len > ReceiveDataBuffer.Length)
                     len = ReceiveDataBuffer.Length;
@@ -58,11 +79,22 @@ namespace CarrotProtocolLib.Service
                     //int len2 = Sp.BytesToRead;
                     //Debug.WriteLine($"BytesToRead2 = {len2}");
                     Device.RxBuffer.Write(ReceiveDataBuffer, 0, len);
-                    Debug.WriteLine($"buflen = {ReceiveDataBuffer.Length}");
+                    Counter += (ulong)readBytes;
+#if DEBUG
+                    Logger.Debug($"{DateTime.Now:HH:mm:ss.ffffff} : read = {readBytes}, counter = {Counter}");
+#endif
+                    //Debug.WriteLine($"{DateTime.Now:HH:mm:ss.fff} : read = {readBytes}, counter = {Counter}");
+
+                    // raw bytes
+                    //byte[] rawBytesArr = new byte[len];
+                    //Array.Copy(ReceiveDataBuffer, rawBytesArr, len);
+                    //var bufBytesStr = BitConverter.ToString(rawBytesArr).Replace("-", "");
+                    //File.AppendAllText("raw.txt", bufBytesStr);
                 }
                 else
                 {
-                    Thread.Sleep(5);
+                    //Thread.Sleep(500);
+                    //Debug.WriteLine($"{DateTime.Now:HH:mm:ss.fff} : sleep 500ms");
                 }
             }
             return 0;
