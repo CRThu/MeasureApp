2c15b7c34c09a8e2731b42eb3c935c89aee9e275
Carrot <11172391+CRThu@users.noreply.github.com>
2024-04-17 17:08:50 +0800

    更新IDriverCommStream

--- IOStreamDemo/IDriverCommStream.cs
+++ IOStreamDemo/IDriverCommStream.cs
@@ -15,16 +15,44 @@ namespace IOStreamDemo
     }
 
     /// <summary>
-    /// 
+    /// 驱动层抽象通信流
     /// </summary>
     public interface IDriverCommStream
     {
+        public DeviceInfo DeviceInfo { get; set; }
+
         public StreamStatus Status { get; set; }
 
+        public ulong RxCount { get; set; }
+        public ulong TxCount { get; set; }
+
+        public delegate void StreamErrorReceivedHandler(Exception ex);
+        public event StreamErrorReceivedHandler? StreamErrorReceived;
+
         /// <summary>
-        /// 获取可用设备列表
+        /// 打开串口
         /// </summary>
-        /// <returns>可用设备信息数组</returns>
-        public abstract static DeviceInfo[] GetDeviceInfos()
+        /// <param name="addr"></param>
+        public abstract void Open(string addr);
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
+        public abstract void Close();
+        /// <summary>
+        /// 写入字节数组
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        public abstract void Write(Span<byte> buffer, int offset, int count);
+        /// <summary>
+        /// 读取字节数组
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="bytesExpected"></param>
+        /// <returns>返回实际读取字节数</returns>
+        public abstract int Read(Span<byte> buffer, int offset, int bytesExpected);
+
     }
 }
