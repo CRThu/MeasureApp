c7e9e86b6510f571ff2ab440d7fac8c89d3340d3
Carrot <11172391+CRThu@users.noreply.github.com>
2024-05-14 11:33:48 +0800

    HighPrecisionTimer重构以及性能测试

---
+++ HighPrecisionTimer/HighPrecisionTimer.cs
@@ -0,0 +1,126 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Media;
+
+namespace HighPrecisionTimer
+{
+    public class HighPrecisionTimer
+    {
+        private static TIMECAPS ptc;
+        private uint uDelay;
+        private uint uResolution;
+        private LPTIMECALLBACK fptc;
+        private uint uTimerID;
+
+        public bool IsRunning { get; private set; }
+
+        public delegate void TickEventHandler(object? sender, TickEventArgs e);
+        public event TickEventHandler? Tick;
+
+        public uint Interval
+        {
+            get
+            {
+                return uDelay;
+            }
+            set
+            {
+                if (value < ptc.wPeriodMin || value > ptc.wPeriodMax)
+                    throw new InvalidOperationException("Invalid Interval");
+                uDelay = value;
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                return IsRunning;
+            }
+            set
+            {
+                if (!IsRunning)
+                {
+                    Start();
+                }
+                else
+                {
+                    Stop();
+                }
+            }
+        }
+
+        static HighPrecisionTimer()
+        {
+            _ = PInvoke.timeGetDevCaps(ptc: out ptc, cbtc: (uint)Marshal.SizeOf(ptc));
+        }
+
+        public HighPrecisionTimer()
+        {
+            IsRunning = false;
+            uDelay = ptc.wPeriodMin;
+            uResolution = ptc.wPeriodMin;
+            fptc = TimerEventCallback;
+            System.Timers.Timer c = new();
+        }
+
+
+        public HighPrecisionTimer(uint interval) : this()
+        {
+            Interval = interval;
+        }
+
+        ~HighPrecisionTimer()
+        {
+            _ = PInvoke.timeKillEvent(uTimerID: uTimerID);
+        }
+
+        public void Start()
+        {
+            if (!IsRunning)
+            {
+                // TIME_ONESHOT
+                // TIME_PERIODIC
+
+                // TIME_CALLBACK_FUNCTION
+                // TIME_CALLBACK_EVENT_SET
+                // TIME_CALLBACK_EVENT_PULSE
+                // TIME_KILL_SYNCHRONOUS
+
+                uTimerID = PInvoke.timeSetEvent(
+                            uDelay: uDelay,
+                            uResolution: uResolution,
+                            fptc: fptc,
+                            dwUser: 0,
+                            fuEvent: PInvoke.TIME_PERIODIC | PInvoke.TIME_KILL_SYNCHRONOUS);
+                if (uTimerID == 0)
+                    throw new InvalidOperationException("Failed to initial Timer");
+                IsRunning = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if (IsRunning)
+            {
+                _ = PInvoke.timeKillEvent(uTimerID: uTimerID);
+                IsRunning = false;
+            }
+        }
+
+        private void TimerEventCallback(uint uTimerID, uint uMsg, nuint dwUser, nuint dw1, nuint dw2)
+        {
+            Tick?.Invoke(this, new TickEventArgs());
+        }
+    }
+
+    public class TickEventArgs : EventArgs
+    {
+        public DateTime SignalTime { get; private set; }
+
+        public TickEventArgs()
+        {
+            SignalTime = DateTime.Now;
+        }
+    }
+}
---
+++ HighPrecisionTimerBenchmark/Program.cs
@@ -0,0 +1,48 @@
+﻿using HdrHistogram;
+using HighPrecisionTimer;
+using System.Diagnostics;
+
+namespace HighPrecisionTimerBenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            int tick = 0;
+            long[] list = new long[1048576];
+            var hist = new LongHistogram(TimeStamp.Hours(1), 3);
+
+            HighPrecisionTimer.HighPrecisionTimer timer = new(1);
+            timer.Tick += (object? sender, TickEventArgs e) =>
+            {
+                list[tick] = Stopwatch.GetTimestamp();
+                tick++;
+            };
+
+            // pre-run
+            timer.Start();
+            Thread.Sleep(10000);
+            timer.Stop();
+
+            // measure
+            tick = 0;
+            timer.Start();
+            Thread.Sleep(60000);
+            timer.Stop();
+
+            Console.WriteLine($"Tick Count: {tick}");
+
+            for (int i = 1; i < tick; i++)
+            {
+                long elapsedNs = (list[i] - list[i - 1]) * 1000L * 1000L * 1000L / Stopwatch.Frequency;
+                hist.RecordValue(elapsedNs);
+            }
+
+            using var writer = new StreamWriter("Hist.hgrm");
+            hist.OutputPercentileDistribution(writer);
+        }
+    }
+}
