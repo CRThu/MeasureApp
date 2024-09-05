91ee7666b115012b431e6aa45825a34a7f75a681
Carrot <11172391+CRThu@users.noreply.github.com>
2024-09-05 16:52:42 +0800

    update ascii_protocol

--- CarrotProtocolSTM32/CarrotProtocolForStm32/Protocol/Inc/ascii_protocol.h
+++ CarrotProtocolSTM32/CarrotProtocolForStm32/Protocol/Inc/ascii_protocol.h
@@ -10,6 +10,8 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "uart_comm.h"
 
 #ifndef _CRT_SECURE_NO_WARNINGS
@@ -20,10 +22,12 @@
 extern "C"
 {
 #endif
-#define ASCII_PROTOCOL_VERSION "1.0.0"
+#define ASCII_PROTOCOL_VERSION "1.0.1"
+
+#define DEBUG_MSG()         protocol_write_msg();
 
     void protocol_parse(uint8_t* buf, uint16_t len, uint16_t* endpos);
-    void protocol_write_msg(char* msg);
+    void protocol_write_msg(const char* format, ...);
     void protocol_write_data(uint8_t* data, uint16_t len);
 
 
--- CarrotProtocolSTM32/CarrotProtocolForStm32/Protocol/Src/ascii_protocol.c
+++ CarrotProtocolSTM32/CarrotProtocolForStm32/Protocol/Src/ascii_protocol.c
@@ -2,22 +2,29 @@
 
 void protocol_parse(uint8_t* buf, uint16_t len, uint16_t* endpos)
 {
-
 }
 
-void protocol_write_msg(char* msg)
+void protocol_write_msg(const char* format, ...)
 {
-    uart_txdma_write((uint8_t*)msg, strlen(msg));
+    char fmt_buf[2048];
+    uint16_t fmt_buf_len;
+    va_list args;
+
+    va_start(args, format);
+    fmt_buf_len = vsnprintf(((char*)&fmt_buf), sizeof(fmt_buf), (char*)format, args);
+    va_end(args);
+
+    uart_txdma_write((uint8_t*)fmt_buf, fmt_buf_len);
 }
 
 void protocol_write_data(uint8_t* data, uint16_t len)
 {
     protocol_write_msg("<data>");
-    protocol_write_msg("<name>databuf</name>");
-    protocol_write_msg("<desc></desc>");
-    protocol_write_msg("<format>binary</format>");
-    protocol_write_msg("<len>0</len>");
-    protocol_write_msg("<word><bits>32</bits></word>");
+    protocol_write_msg("<name>%s</name>", "databuf");
+    protocol_write_msg("<desc>%s</desc>", "NULL");
+    protocol_write_msg("<format>%s</format>", "binary");
+    protocol_write_msg("<len>%lu</len>", len);
+    protocol_write_msg("<word><bits>%d</bits></word>", 32);
     protocol_write_msg("<bin>");
     uart_txdma_write(data, len);
     protocol_write_msg("</bin>");
