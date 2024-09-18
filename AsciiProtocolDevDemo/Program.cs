7bd8acdb99b49582139b659b87830fc9c7206c02
Carrot <11172391+CRThu@users.noreply.github.com>
2024-09-18 17:10:42 +0800

    解析更新数据包

---
+++ AsciiProtocolDevDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using CarrotCommFramework.Protocols;
+using System.Buffers;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace AsciiProtocolDevDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var crlf = "\r\n".AsSpan();
+            var dataTagStart = "<data>".AsSpan();
+            var dataTagEnd = "</data>".AsSpan();
+
+            string rdStr = "COMMAND1_PLACEHOLDER\r\n<data>DATA_PLACEHOLDER</data>\r\nCOMMAND2_PLACEHOLDER\r\n";
+            Console.WriteLine($"BUF:{rdStr.ReplaceLineEndings("\\r\\n")}");
+            var rdSeq = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes(rdStr.ToCharArray()));
+            RawAsciiProtocol asciiProtocol = new();
+            bool ret = asciiProtocol.TryParse(ref rdSeq, out var pkts);
+            if (ret)
+            {
+                foreach (var pkt in pkts)
+                {
+                    Console.WriteLine($"PKTMSG:{pkt.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO PACKETS CAN BE PARSED");
+            }
+            return;
+
+            /*
+            while (true)
+            {
+                string rd = Console.ReadLine();
+                var rdSeq = new ReadOnlySequence<char>(rd.ToCharArray());
+                var rdSeqR = new SequenceReader<char>(rdSeq);
+                if (rdSeqR.TryReadTo(out ReadOnlySequence<char> seq, dataTagStart, true))
+                {
+                    Console.WriteLine($"Read To:{new string(seq.ToArray())}");
+                    if (rdSeqR.TryReadTo(out seq, dataTagEnd, true))
+                    {
+                        Console.WriteLine($"Read To:{new string(seq.ToArray())}");
+                    }
+                }
+                else if (rdSeqR.TryReadTo(out seq, crlf, true))
+                {
+                    Console.WriteLine($"Read To:{new string(seq.ToArray())}");
+                    Console.WriteLine($"Unread:{new string(rdSeqR.UnreadSequence.ToArray())}");
+                }
+                else
+                    Console.WriteLine($"Cannot find eof element");
+            }
+
+            */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- CarrotCommFramework/Protocols/RawAsciiProtocol.cs
+++ CarrotCommFramework/Protocols/RawAsciiProtocol.cs
@@ -19,21 +19,45 @@ namespace CarrotCommFramework.Protocols
         public override bool TryParse(ref ReadOnlySequence<byte> buffer, out IEnumerable<Packet>? packets)
         {
             List<Packet> packetsList = new();
+            var startTag = "<data>".AsciiToBytes().AsSpan();
+            var endTag = "</data>".AsciiToBytes().AsSpan();
+            var crlfTag = "\r\n".AsciiToBytes().AsSpan();
+            var reader = new SequenceReader<byte>(buffer);
+            ReadOnlySequence<byte> seq;
+            ReadOnlySequence<byte> seq2;
 
             // 处理数据流直到不完整包或结束
             while (true)
             {
-                SequencePosition? pos = buffer.PositionOf((byte)'\n');
-
-                // 不完整包结构则结束
-                if (!pos.HasValue)
+                if (reader.TryReadTo(out seq, crlfTag, true))
                 {
+                    var seqRder = new SequenceReader<byte>(seq);
+                    if (seqRder.TryReadTo(out seq2, startTag, true))
+                    {
+                        if (!seq2.IsEmpty)
+                        {
+                            Console.WriteLine($"Read ERROR:{BytesEx.BytesToAscii(seq2.ToArray())}");
+                        }
+                        if (seqRder.TryReadTo(out seq2, endTag, true))
+                        {
+                            Console.WriteLine($"Read To:{BytesEx.BytesToAscii(seq2.ToArray())}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Read To:{BytesEx.BytesToAscii(seq.ToArray())}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot find eof element");
                     break;
                 }
 
-                RawAsciiProtocolPacket pkt = new(buffer.Slice(buffer.Start, pos.Value).ToArray());
-                buffer = buffer.Slice(buffer.GetPosition(1, pos.Value));
-                packetsList.Add(pkt);
+
+                //RawAsciiProtocolPacket pkt = new(buffer.Slice(buffer.Start, pos.Value).ToArray());
+                //buffer = buffer.Slice(buffer.GetPosition(1, pos.Value));
+                //packetsList.Add(pkt);
             }
 
             packets = packetsList;
