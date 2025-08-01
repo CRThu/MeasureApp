ec7ceaa33fc75dbe8720c3327713bb79959d9cc6
Carrot <11172391+CRThu@users.noreply.github.com>
2025-08-01 14:12:52 +0800

    update scriptViewer

---
+++ MeasureApp/Model/ScriptViewer.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MeasureApp.Model
+{
+    public partial class ScriptViewer : ObservableObject
+    {
+        [ObservableProperty]
+        public string header;
+
+        [ObservableProperty]
+        public string text;
+    }
+}
--- MeasureApp/ViewModel/ScriptRunnerVM.cs
+++ MeasureApp/ViewModel/ScriptRunnerVM.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MeasureApp.Model;
+using MeasureApp.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MeasureApp.ViewModel
 {
     public partial class ScriptRunnerVM : BaseVM
     {
+        [ObservableProperty]
+        private ObservableCollection<ScriptViewer> scriptViewers = new()
+        {
+            new ScriptViewer() { Header = "Tab1", Text = "Text1" },
+            new ScriptViewer() { Header = "Tab2", Text = "Text2" }
+        };
 
+        [RelayCommand]
+        public void Closed()
+        {
+            MessageBox.Show("Closed Called");
+        }
     }
 }
